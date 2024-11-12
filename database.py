6a670b1684c0189a5da9b1dc4df7afbaa5280dc9
Adishree Bhuyan <130736664+goandamb09@users.noreply.github.com>
2024-11-13 01:43:51 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQL_ALCHEMY_DATABASE_URL='postgresql://postgres:lol@localhost/fastapi'
+
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit = False,autoflush=False,bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
